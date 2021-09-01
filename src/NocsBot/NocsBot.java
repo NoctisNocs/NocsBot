c926888739c243f821e2bb0cf760bf6bd04a39a4
NoctisNocs <NoctisNocs@gmail.com>
2021-09-01 19:56:51 +0100

    Version 0.1, bot framework skeleton.

---
+++ src/NocsBot/NocsBot.java
@@ -0,0 +1,23 @@
+package NocsBot;
+import org.osbot.rs07.script.Script;
+import org.osbot.rs07.script.ScriptManifest;
+
+@ScriptManifest(info = "Eat ass, skate fast.", logo = "https://i.imgur.com/KHlIsUE.jpg", version = 1.0, author = "Noc", name = "NocsBot")
+public class NocsBot extends Script
+{
+    /*
+    @Override
+    public void onStart() throws InterruptedException {
+        super.onStart();
+    }
+
+    @Override
+    public void onExit() throws InterruptedException {
+        super.onExit();
+    }*/
+
+    @Override
+    public int onLoop() throws InterruptedException {
+        return 0;
+    }
+}
